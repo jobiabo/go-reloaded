a0c5b959c2a71a3b500dd6674a5bd8e110c145aa
jobiabo <joelobiabo@gmail.com>
2026-03-14 14:10:18 +0000

    my reloadstagesfinal

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"helper/helper"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 3 {
+		fmt.Println("commandline insufficient")
+		return
+	}
+
+	inputFile := args[1]
+	outputFile := args[2]
+
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("error reading file", err)
+		return
+	}
+
+	sampleIn := string(data)
+
+	sampleIn = helper.CapAlphaMod(sampleIn)
+	sampleIn = helper.HexBin(sampleIn)
+	sampleIn = helper.PunctAAte(sampleIn)
+	sampleIn = helper.Vowels(sampleIn)
+
+	err = os.WriteFile(outputFile, []byte(sampleIn), 0644)
+	if err != nil {
+		fmt.Println("error writing file", err)
+		return
+	}
+
+}
